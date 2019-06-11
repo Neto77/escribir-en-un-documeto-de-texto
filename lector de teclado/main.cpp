e194b7130fe25d9a60729650cee990ff0089a96b
Sett <35352092+Neto77@users.noreply.github.com>
2019-06-11 00:58:07 -0500

    escribir en un rachivo de texto codigo base para detectar el teclado mas adelante

---
+++ lector de teclado/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
+
+void escribir();
+
+int main(){
+
+    escribir();
+
+
+    system("pause");
+    return 0;
+}
+
+void escribir(){
+    ofstream archivo;
+
+    string nombreArchivo,frase;
+    cout<<"Digite el nombre del archivo despues agregue un puto y extencion txt: ";
+    getline(cin,nombreArchivo);
+
+    archivo.open(nombreArchivo.c_str() ,ios::out);//abriendo el archivo
+    if(archivo.fail()){
+        cout<<"No se pudo crear el archivo";
+        exit(1);
+    }
+    cout<<"\nDigite el texto del archivo: ";
+    getline(cin,frase);
+    archivo<<frase;
+
+    archivo.close();//cerrar el archivo
+}
